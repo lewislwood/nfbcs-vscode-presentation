5dca0841cd6497a8499bed0a0e68bfd93bbf2bc6
Lewis Wood <lewislwood@gmail.com>
2025-12-22 21:42:33 -0600

    Audio tutorials mods

--- api/src/articles/articles.ts
+++ api/src/articles/articles.ts
@@ -90,6 +90,47 @@ Individual articles can be copied/downloaded below:
 * [thunder_client.md](https://raw.github.com/lewislwood/nfbcs-vscode-presentation/master/api/src/articles/md/thunder_client.md)
         `
         },
+        {
+            id: 5,
+            title: "Audio Tutorials",
+            markdown: `# Audio Tutorials
+
+Here are a few audio tutorials for Vs Code. I demonstrate and talke about these features.  I hope these help.
+
+* [Typical coding and navigating](https://raw.github.com/lewislwood/nfbcs-vscode-presentation/master/media/Typical_coding_and_navigating.mp3)
+
+Below are my original recordings back around 2020 when Vs Code was young as far as accessibility goes. Accessible buffer is now called accessible viewer and alt + f2 invokes it from terminal, chat and other places. When I recorded I dod not do the class viewer and oversight on my part.
+
+* [Part 1 Welcome](https://raw.github.com/lewislwood/nfbcs-vscode-presentation/master/media/Part_1_Welcome.mp3)
+
+* [Part 2 settings, cmd palette, file explorer](https://raw.github.com/lewislwood/nfbcs-vscode-presentation/master/media/part_2_settings_cmd_palette_file_explorer.mp3)
+
+* [Part 3 Outline and brace Navigation](https://raw.github.com/lewislwood/nfbcs-vscode-presentation/master/media/Part_3_Outline_and_brace_Navigation.mp3)
+
+* [Part 4 Split Editor Groups  Anchor Selection](https://raw.github.com/lewislwood/nfbcs-vscode-presentation/master/media/Part_4_Split_Editor_Groups_Anchor_Selection.mp3)
+
+* [Pt 5 find replace text selection](https://raw.github.com/lewislwood/nfbcs-vscode-presentation/master/media/Pt_5_find_replace_text_selection.mp3)
+
+* [Pt 6 New project emmet, html doc, label, inputHtml, Emmet label input](https://raw.github.com/lewislwood/nfbcs-vscode-presentation/master/media/Pt_6_New_project_emmet_html_doc_label_inputHtml_Emmet_label_input.mp3)
+
+* [Pt 7 Emmet  comboboxp](https://raw.github.com/lewislwood/nfbcs-vscode-presentation/master/media/Pt_7_Emmet_combobox.mp3)
+
+* [Pt 8 Emmet List items](https://raw.github.com/lewislwood/nfbcs-vscode-presentation/master/media/Pt_8_Emmet_List_items.mp3)
+
+* [Pt 9a a Emmet  Table with Column Row Headings](<https://raw.github.com/lewislwood/nfbcs-vscode-presentation/master/media/Pt_9a_a_Emmet>_Table_with_Column_Row_Headings.mp3)
+
+* [Pt 9b Emmet Table ColSpan Headings](https://raw.github.com/lewislwood/nfbcs-vscode-presentation/master/media/Pt_9b_Emmet_Table_ColSpan_Headings.mp3)
+
+* [Pt 10a  Snippet Creation Prep Work](https://raw.github.com/lewislwood/nfbcs-vscode-presentation/master/media/Pt_10a_Snippet_Creation_Prep_Work.mp3)
+
+* [Pt 10b Snippet File creation test](https://raw.github.com/lewislwood/nfbcs-vscode-presentation/master/media/Pt_10b_Snippet_File_creation_test.mp3)
+
+* [Pt 10c Snippet multi input block reusability demoed.mp](https://raw.github.com/lewislwood/nfbcs-vscode-presentation/master/media/Pt_10c_Snippet_multi_input_block_reusability_demoed.mp3)
+
+* [Pt 10d Snippets Inline Choices and variables assigning key](https://raw.github.com/lewislwood/nfbcs-vscode-presentation/master/media/Pt_10d_Snippets_Inline_Choices_and_variables_assigning_key.mp3)
+
+`
+        },
 {    
     id: 2,
     title: "md Read Me",
